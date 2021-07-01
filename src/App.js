0a3edcf1c8f11112a35f4246315192bf65513bc5
user name <user email>
2021-07-01 13:56:41 +0530

    Ability to go to website on clicking the logo

--- src/App.js
+++ src/App.js
@@ -12,17 +12,22 @@ import logo from './Assets/logo.png';
 //   )
 // }
 class App extends React.Component {
+  url = 'https://www.bridgelabz.com/'
   constructor() {
     super();
     this.state = {
       title: "Hello From Bridgelab"
     }
   }
+  onClick = ($event) => {
+    console.log("Button is clicked ",$event)
+    window.open(this.url,"_blank");
+  }
   render() {
     return (
       <div>
         <h1>{this.state.title}</h1>
-        <img src = {logo}/>
+        <img src = {logo} onClick = {this.onClick}/>
       </div>
     );
   }
