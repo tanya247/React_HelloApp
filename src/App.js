32b9beec3e4d4f730a2bd179ca307bb26fd36217
user name <user email>
2021-07-01 12:57:53 +0530

    Ability to do One way Binding

--- src/App.js
+++ src/App.js
@@ -3,13 +3,27 @@ import './App.css';
 import React from 'react';
 
 
-class App extends React.Component{
-  render(){
-    return(
+//class App extends React.Component {
+// render(){
+//   return(
+//     <div>
+//       <h1>Hello From Bridgelabz</h1>
+//     </div>
+//   )
+// }
+class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      title: "Hello From Bridgelab"
+    }
+  }
+  render() {
+    return (
       <div>
-        <h1>Hello From Bridgelabz</h1>
+        <h1>{this.state.title}</h1>
       </div>
-    )
+    );
   }
 }
 export default App;
